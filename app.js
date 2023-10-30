10ed74f234389645c07a968557ba8a428d1ca463
Xurshid97 <fantasticidea3000@gmail.com>
2023-10-30 21:44:34 +0100

    All task are done

--- app.js
+++ app.js
@@ -35,21 +35,19 @@ class PianoRollDisplay {
 
     // Append the SVG to the card container
     cardDiv.appendChild(svg);
-
     return { cardDiv, svg }
   }
 
   async generateSVGs() {
     if (!this.data) await this.loadPianoRollData();
     if (!this.data) return;
-    
+
     const pianoRollContainer = document.getElementById('pianoRollContainer');
     pianoRollContainer.innerHTML = '';
     for (let it = 0; it < 20; it++) {
       const start = it * 60;
       const end = start + 60;
       const partData = this.data.slice(start, end);
-
       const { cardDiv, svg } = this.preparePianoRollCard(it)
 
       pianoRollContainer.appendChild(cardDiv);
@@ -61,4 +59,167 @@ class PianoRollDisplay {
 document.getElementById('loadCSV').addEventListener('click', async () => {
   const csvToSVG = new PianoRollDisplay();
   await csvToSVG.generateSVGs();
+
+  // after all cards being rendered function call is made to work on them
+  getPianoRollEls()
 });
+
+
+
+
+function getPianoRollEls() {
+  // select all the cards to work on them
+  let pianoRollCards = document.querySelectorAll('.piano-roll-card')
+  pianoRollCards.forEach((pianoRollCardEl) => {
+    setMainPianoRoll(pianoRollCardEl)
+  })
+}
+
+
+// when piano-roll-card is clicked remind it
+let lastMainPianoRollCard = null;
+function setMainPianoRoll(pianoRollCardEl) {
+
+  // get elements from html to change screen shape and children elements
+  let screenChangeEl = document.getElementById('screenChange')
+  let pianoRollContainerMainEl = document.getElementById('pianoRollContainerMain')
+  let pianoRollContainerEl = document.getElementById('pianoRollContainer')
+
+
+  pianoRollCardEl.addEventListener('click', () => {
+
+    // check if the last modiefied card exist, and remove mainCard class from it
+    if (lastMainPianoRollCard) {
+      lastMainPianoRollCard.classList.remove('mainPianoRollCard')
+    }
+
+    // on each card clicked, change screen shape and
+    screenChangeEl.id = 'screenChangeUpdated'
+
+    // add pianoRollContainerMainUpdate class to make a parent of card and set it empty
+    pianoRollContainerMainEl.id = 'pianoRollContainerMainUpdate'
+    pianoRollContainerMainEl.innerHTML = ''
+
+    // add pianoRollContainerUpdate class to make vertical screen for cards
+    pianoRollContainerEl.id = 'pianoRollContainerUpdate'
+
+    // update clicked card class name
+    pianoRollCardEl.classList.add('mainPianoRollCard')
+
+    // get the clone of the clicked card, and add it to main container element
+    let pianoRollCardChildEl = pianoRollCardEl.cloneNode(true);
+    pianoRollContainerMainEl.appendChild(pianoRollCardChildEl)
+
+    // remember last modified piano Card
+    lastMainPianoRollCard = pianoRollCardEl
+    getSelectedPart(pianoRollContainerMainEl.querySelector('.piano-roll-svg'))
+  })
+}
+
+/* get Main piano Card and select part of it */
+function getSelectedPart(mainPianoCard) {
+
+  let isSelecting = false;
+  let startCoords = { x: 0, y: 0 };
+  let endCoords = { x: 0, y: 0 };
+
+
+  let selectionRectangle = document.getElementById("selection-rectangle");
+
+  mainPianoCard.addEventListener("mousedown", (e) => {
+    isSelecting = true;
+    startCoords.x = e.clientX;
+    startCoords.y = e.clientY;
+    let selectionRectangle = document.getElementById("selection-rectangle");
+    selectionRectangle.innerText = ''
+  });
+
+  mainPianoCard.addEventListener("mousemove", (e) => {
+    if (isSelecting) {
+      endCoords.x = e.clientX;
+      endCoords.y = e.clientY;
+      updateSelectionRectangle();
+    }
+  });
+
+  let lastModifiedNotes = null;
+  mainPianoCard.addEventListener("mouseup", () => {
+    isSelecting = false;
+    lastModifiedNotes = selectCellsInRectangle();
+  });
+
+  function updateSelectionRectangle() {
+    selectionRectangle.style.display = "block";
+
+    const left = Math.min(startCoords.x, endCoords.x);
+    const top = Math.min(startCoords.y, endCoords.y);
+    const width = Math.abs(startCoords.x - endCoords.x);
+    const height = Math.abs(startCoords.y - endCoords.y);
+
+    selectionRectangle.style.left = left + "px";
+    selectionRectangle.style.top = top + "px";
+    selectionRectangle.style.width = width + "px";
+    selectionRectangle.style.height = height + "px";
+    
+
+    /* before every update rectangle area drawn, get last modified notes and remove the class name */
+    if(lastModifiedNotes) {
+      lastModifiedNotes.forEach((lastModifiedNote)=> {
+        lastModifiedNote.classList.remove('selected-note')
+      })
+    }
+
+  }
+
+
+  function selectCellsInRectangle() {
+    let selectionRectangle = document.getElementById("selection-rectangle");
+
+    // get size and position of each note Selection rectangle
+    const selectionRect = selectionRectangle.getBoundingClientRect();
+
+    const selectionRectLeft = selectionRect.left;
+    const selectionRectTop = selectionRect.top;
+    const selectionRectRight = selectionRect.right;
+    const selectionRectBottom = selectionRect.bottom;
+
+    // Select only Main updated container's notes, helps faster search
+    let pianoRollContainerMainUpdateEl = document.getElementById('pianoRollContainerMainUpdate')
+    const noteRectangles = pianoRollContainerMainUpdateEl.querySelectorAll('.note-rectangle');
+    let noteCounter = 0;
+    let lastModifiedNotes = []
+    
+    noteRectangles.forEach((noteRectangle) => {
+
+      // get size and position of each note
+      const noteRect = noteRectangle.getBoundingClientRect();
+
+      const noteRectLeft = noteRect.left;
+      const noteRectTop = noteRect.top;
+      const noteRectRight = noteRect.right;
+      const noteRectBottom = noteRect.bottom;
+
+      // if note is inside of the selected rectangle area
+      if (
+        noteRectLeft >= selectionRectLeft &&
+        noteRectTop >= selectionRectTop &&
+        noteRectRight <= selectionRectRight &&
+        noteRectBottom <= selectionRectBottom
+      ) {
+        noteCounter ++
+        // add selected-note class to get what I selected
+        noteRectangle.classList.add('selected-note');
+
+        // remember last modified notes to remove above class
+        lastModifiedNotes.push(noteRectangle)
+      }
+    });
+
+    selectionRectangle.textContent = `number of notes: ${noteCounter}`
+    console.log('number of notes:', noteCounter);
+    return lastModifiedNotes
+    
+  }
+
+}
+
